7ea36379077940606c6997b6713dd72aa64585cf
1exz <alex.buchko.m@gmail.com>
2022-01-05 23:55:42 +0200

    app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'behaviorSubject';
-}
+export class AppComponent implements OnInit {
+  result:number=0;
+    ngOnInit() {
+    let subject = new BehaviorSubject(5);
+    function random(){
+    const rand = Math.round(Math.random() * 1000);
+     subject.next(rand);
+    }
+     setInterval(random,3000);
+     subject.subscribe(console.log);
+     subject.subscribe((result) => {
+      if (result >= 300 && result <= 700) {
+        this.result = result;
+      }
+    });
+    }
+  }
